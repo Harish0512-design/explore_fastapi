747a7d5c67c23a5aa69a63f4f87d265435a7ef29
Harish <harishsomsole@gmail.com>
2024-01-29 19:10:52 +0530

    Path Params

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users/{userid}")
+async def get_users(userid: int):
+    return {"userid": userid}
+
+
+@app.get("/users/str/{userid}")
+async def get_str_user(userid: str):
+    return {"userid": userid}
