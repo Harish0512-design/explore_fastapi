65e9c5abc5d15c82729f3c6bdfc2a94d43c912d5
Harish <harishsomsole@gmail.com>
2024-01-29 19:19:05 +0530

    Path Params with ENUMS

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -16,3 +18,25 @@ async def get_users(userid: int):
 @app.get("/users/str/{userid}")
 async def get_str_user(userid: str):
     return {"userid": userid}
+
+
+class ModelBase(str, Enum):
+    nlp = "nlp"
+    dl = "dl"
+    ml = "ml"
+
+
+@app.get("/courses/{course_id}")
+async def get_course(course_id: str):
+
+    if course_id == ModelBase.nlp:
+        return {"course": "You bought Natural Language Processing Course"}
+
+    elif course_id == ModelBase.dl:
+        return {"course": "You bought Deep Learning Course"}
+
+    elif course_id == ModelBase.ml:
+        return {"course": "You bought Machine Learning Course"}
+
+    else:
+        return {"course": "Invalid Course Id"}
